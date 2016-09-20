b9501e4f6a89a6bcb3dc153a8f46f4c6ad0797a9
justed <dev.justed@gmail.com>
2016-09-21 00:28:51 +0500

    more useless tests

--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -19,9 +19,33 @@ describe 'Car' do
       car.color = 'foo'
       expect(car.color).to eq('foo')
     end
+
+    it 'allows read wheels' do
+      car = Car.new
+      expect(car.wheels).to eq(4)
+    end
+
+    it 'allows writing for doors'
   end
 
-  describe '.colors'
+  describe '.colors' do
+    it 'returns an array of colors' do
+      c = %w(blue black red green)
+      expect(Car.colors).to match_array(c)
+    end
+  end
 
-  describe '#full_name'
+  describe '#full_name' do
+    it 'returns it in the expected format' do
+      @honda = Car.new(make: 'Honda', year: 2004, color: 'blue')
+      expect(@honda.full_name).to eq('2004 Honda (blue)')
+    end
+
+    context 'when initialized with no arguments' do
+      it 'returns a string using default values' do
+        car = Car.new
+        expect(car.full_name).to eq('2007 Volvo (unknown)')
+      end
+    end
+  end
 end
